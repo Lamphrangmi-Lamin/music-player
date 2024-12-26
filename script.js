898abd926a6746fc0aa89c9cc87c778dd930a778
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2024-12-26 15:01:25 +0530

    fixing bugs of track-picker, seekSlider

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 const audio = document.getElementById('current-track');
 const playPauseIcon = document.getElementById('play-pause');
-const seeklSlider = document.getElementById('seek-slider');
+const seekSlider = document.getElementById('seek-slider');
 const duration = document.querySelector('.total-duration');
 const currentTime = document.querySelector('.current-time');
 const title = document.querySelector('.title');
-const artist = document.querySelector('.artist');
 const albumCover = document.querySelector('.album-cover');
+const trackPicker =  document.getElementById('track-picker');
 
 // initially display the currentTime
 currentTime.textContent = formatTime(audio.currentTime);
-duration.textContent = formatTime(audio.duration);
-seeklSlider.max = audio.duration;
 
 // function to update the currentTime
-
 function formatTime(time) {
     let minutes = Math.floor(time / 60);
     let seconds = Math.floor(time % 60);
     return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
-// function updateCurrentTime() {
-//     currentTime.textContent = Number(audio.currentTime).toFixed();
-//     seeklSlider.value = Number(audio.currentTime);
-// }
 
 function updateCurrentTime() {
     currentTime.textContent = formatTime(audio.currentTime);
-    seeklSlider.value = Number(audio.currentTime);
+    seekSlider.value = Number(audio.currentTime);
 }
 
 // adding functionality to the seek-slider
-seeklSlider.addEventListener('input', (e) => {
-    currentTime.textContent = e.target.value;
-    audio.currentTime = e.target.value;
-    audio.play();
+seekSlider.addEventListener('input', (e) => {
+    const newTime = e.target.value;
+    audio.currentTime = newTime;
+    currentTime.textContent = formatTime(newTime);
+    
+})
+
+// seekSlider change event
+seekSlider.addEventListener('change', (e) => {
+    if (!audio.paused) {
+        audio.play();
+    }
 })
 
 // adding functionality to the play-pause button
@@ -42,8 +43,6 @@ playPauseIcon.addEventListener('click', playPause);
 
 function playPause() {
     if (audio.paused) {
-        title.textContent = 'IIT BOMBAY MOVIE';
-        artist.textContent = 'NITISH KUMAR';
         playPauseIcon.src = './icons/pause-solid.svg';
         audio.play();
         audio.addEventListener('timeupdate', updateCurrentTime);
@@ -52,3 +51,20 @@ function playPause() {
         audio.pause();
     }
 }
+
+// adding functionality to the track-picker
+trackPicker.addEventListener('change', (e) => {
+    audio.pause();
+    const file = e.target.files[0];
+    const url = URL.createObjectURL(file);
+    audio.src = url;
+    title.textContent = file.name;
+    audio.load();
+    // update metadata after loading
+    audio.addEventListener('loadedmetadata', () => {
+        duration.textContent = formatTime(audio.duration);
+        seekSlider.max = audio.duration;
+        currentTime.textContent = formatTime(audio.currentTime);
+        seekSlider.value = Number(audio.currentTime);
+    })
+})
